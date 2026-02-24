cea2daaf96bdd14eac9d32176498ce814b5a85b4
GrScode404 <grigoryev.nhpt@gmail.com>
2026-02-24 13:56:09 +0300

    fixed app.py

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__) # NOSONAR
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
