239bcca7e2492362bb4689181975721a07ae0869
GrScode404 <grigoryev.nhpt@gmail.com>
2026-02-25 13:15:09 +0300

    fixed test_app.py

--- page_analyzer/tests/test_app.py
+++ page_analyzer/tests/test_app.py
@@ -1,11 +1,14 @@
 import pytest
+
 from page_analyzer import app
+
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+
 
 def test_index(client):
     response = client.get('/')
