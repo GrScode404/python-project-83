3bfe8e36bd8850be2f65359c219df23f1e17bf0d
GrScode404 <grigoryev.nhpt@gmail.com>
2026-02-26 10:54:48 +0300

    connected db, turned off sonar coverage

--- Makefile
+++ Makefile
@@ -16,8 +16,8 @@ fixlint:
 test:
 	uv run pytest
 
-test-coverage:
-	uv run pytest --cov=page_analyzer --cov-report=xml:coverage.xml
+# test-coverage:
+# 	uv run pytest --cov=page_analyzer --cov-report=xml:coverage.xml
 
 start:
 	uv run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template
 
+from page_analyzer.db import get_connection
+
 app = Flask(__name__)  # NOSONAR
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/test-db')
+def test_db():
+    conn = get_connection()
+    conn.close()
+    return "DB OK"
