26b90e6379d0ca8c145583fcd67c268ee1c17911
GrScode404 <grigoryev.nhpt@gmail.com>
2026-02-27 11:56:53 +0300

    fixed db.py

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL не установлена!")
 
 def get_connection():
     return psycopg2.connect(DATABASE_URL)
